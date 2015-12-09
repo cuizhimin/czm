1ede0b9959e541a3bb334ecdf2c05c257dd6159a
cuizhimin <inocent@foxmail.com>
2015-12-09 19:18:17 +0800

    sort
    从小到大排序。

---
+++ main.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+//  czm
+//
+//  Created by czm on 15/11/25.
+//  Copyright © 2015年 czm. All rights reserved.
+//
+
+#include <iostream>
+#define N 5
+
+int main(int argc, const char * argv[])
+{
+    // insert code here...
+    int a[N];
+    int i,j,temp;
+    for (i=0;i<N;i++)
+        scanf("%d",&a[i]);
+    for(i=0;i<N-1;i++)
+    {
+    for(j=0;j<N-1-i;j++)
+     {
+    if(a[j]>a[j+1])
+      { temp=a[j];
+        a[j]=a[j+1];
+        a[j+1]=temp;}
+     }
+    }
+    for(i=0;i<N;i++)
+        printf("%d\n",a[i]);
+    free(a);
+    return 0;
+}
